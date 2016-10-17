303e658f84429f3cfc6ecb8d55d7fc72603aadf1
Evgeniy Plyusnin <eninbox@gmail.com>
2016-10-17 17:30:27 +0500

    Какая-то организация Lifetime-а

---
+++ LifetimeDefinition.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lifetime
+{
+    public interface ILifeTime
+    {
+        void Add(Action OnTerminate);
+    }
+
+    public class LifeTime : ILifeTime
+    {
+        private readonly List<Action> _onTerminate = new List<Action>();
+
+        public void Add(Action OnTerminate) { _onTerminate.Add(OnTerminate); }
+
+        public void Terminate()
+        {
+            foreach (Action terminateAction in Enumerable.Reverse(_onTerminate))
+                terminateAction();
+        }
+    }
+
+    public static class LifeTimes
+    {
+        private static readonly LifeTime _eternal = new LifeTime();
+
+        public static LifeTime Eternal
+        {
+            get { return _eternal; }
+        }
+
+        public static ILifetimeDefinition Define(LifeTime Parent)
+        {
+            var def = new LifetimeDefinition(new LifeTime());
+            Parent.Add(def.Dispose);
+            return def;
+        }
+    }
+
+    public interface ILifetimeDefinition : IDisposable
+    {
+        LifeTime LifeTime { get; }
+    }
+
+    public class LifetimeDefinition : ILifetimeDefinition
+    {
+        private readonly LifeTime _lifeTime;
+
+        public LifetimeDefinition(LifeTime LifeTime) { _lifeTime = LifeTime; }
+
+        public LifeTime LifeTime
+        {
+            get { return _lifeTime; }
+        }
+
+        public void Dispose() { LifeTime.Terminate(); }
+    }
+}
